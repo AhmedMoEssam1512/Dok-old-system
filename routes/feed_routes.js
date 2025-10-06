0b61b9714b388fe3a765e8c58ea124c7ed72b9ed
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-10-06 16:08:00 +0300

    ..

--- routes/feed_routes.js
+++ routes/feed_routes.js
@@ -5,7 +5,7 @@ const feedMiddleware = require('../middleware/feed_middleware.js');
 const auth = require('../middleware/auth_middleware');
 
 router.route('/')
-    .get(auth.adminProtect ,feedMiddleware.deletePostsGreaterThan14Days ,feedController.getFeed);
+    .get(auth.protect ,feedMiddleware.deletePostsGreaterThan14Days ,feedController.getFeed);
 
 router.route('/postOnFeed')
     .post(auth.adminProtect, feedController.postOnFeed);
